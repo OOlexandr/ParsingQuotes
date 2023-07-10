93892d8794e15e1218cbcdd874af72b871b5c7ba
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-07-10 11:52:01 +0300

    pages

--- main.py
+++ main.py
@@ -28,6 +28,21 @@ def parse_author(soup):
         'description': description}
     return author_dict
 
+def parse_page(page, found_authors, url):
+    quotes = []
+    authors = []
+    containers = page.find_all('div', class_='quote')
+    for c in containers:
+        quote, ref = parse_quote(c)
+        quotes.append(quote)
+        if not quote['author'] in found_authors:
+            found_authors.append(quote['author'])
+            author_url = url + ref
+            author_response = requests.get(author_url)
+            author = parse_author(BeautifulSoup(author_response.text, 'lxml'))
+            authors.append(author)
+    return quotes, authors
+
 def main():
     url = 'https://quotes.toscrape.com/'
     response = requests.get(url)
@@ -36,17 +51,17 @@ def main():
     quotes = []
     authors = []
     found_authors = []
-    containers = soup.find_all('div', class_='quote')
-
-    for c in containers:
-        quote, ref = parse_quote(c)
-        if not quote['author'] in found_authors:
-            found_authors.append(quote['author'])
-            author_url = url + ref
-            author_response = requests.get(author_url)
-            author = parse_author(BeautifulSoup(author_response.text, 'lxml'))
-            quotes.append(quote)
-            authors.append(author)
+    next_page = soup.find('li', class_='next')
+    while next_page:
+        new_quotes, new_authors = parse_page(soup, found_authors, url)
+        quotes += new_quotes
+        authors += new_authors
+        page = requests.get(url+next_page.find('a').attrs["href"])
+        soup = BeautifulSoup(page.text, 'lxml')
+        next_page = soup.find('li', class_='next')
+    new_quotes, new_authors = parse_page(soup, found_authors, url)
+    quotes += new_quotes
+    authors += new_authors
     
     with open('authors.json', 'w') as a:
         json.dump(authors, a)
